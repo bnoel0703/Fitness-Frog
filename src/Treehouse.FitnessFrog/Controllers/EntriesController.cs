8c14cc32bd2661d0038f10e2f0e7de0f6e7244b0
bnoel0703 <32888299+bnoel0703@users.noreply.github.com>
2018-04-12 15:45:34 -0400

    Added validation summary section

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -44,7 +44,7 @@ namespace Treehouse.FitnessFrog.Controllers
             var entry = new Entry()
             {
                 Date = DateTime.Today,
-                ActivityId = 2
+                ActivityId = 0
             };
 
             ViewBag.ActivitiesSelectListItems = new SelectList(Data.Data.Activities, "Id", "Name");
@@ -56,6 +56,7 @@ namespace Treehouse.FitnessFrog.Controllers
         [HttpPost]
         public ActionResult Add(Entry entry)
         {
+
             // If there aren't any "Duration" field validation errors
             // then make sure that the duration is greater than "0".
             if (ModelState.IsValidField("Duration") && entry.Duration <= 0)
